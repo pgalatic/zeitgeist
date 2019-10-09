852aaf2924a3eb3519addef0086aefcdcb5b1585
theodorabendlin <trb7281@cs.rit.edu>
2019-10-09 11:48:20 -0400

    merged from master

--- purify.py
+++ purify.py
@@ -1,6 +1,6 @@
 # author: Paul Galatic
 #
-# Program to preprocess Twitter text data and create 'blob files' that contain 
+# Program to preprocess Twitter text data and create 'blob files' that contain
 # only the text.
 #
 
@@ -34,17 +34,18 @@ def cleanse(target):
             with open(str(DATA_DIR / target) + '.csv.', 'w', newline='', encoding='utf-8') as dst_file:
                 rdr = csv.reader(src_file)
                 wtr = csv.writer(dst_file)
-                
+
                 for row in rdr:
                     length_before += len(row[1])
-                    
+
                     # Block 1 removes '&amp;' as it is a specific nuisance. 
-                    # Block 2 of this RE removes all usernames. 
-                    # Block 3 removes all punctuation and non-alphanumeric 
+                    # Block 2 of this RE removes all usernames.
+                    # Block 3 removes all punctuation and non-alphanumeric
                     #   characters (except apostrophes, @ tags and hashtags).
                     # Block 4 removes URLs.
                     # text = re.sub('(&amp;)|(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)', ' ', row[1])
                     # Currently only using blocks 1, 3, and 4. TODO: Do we want to remove usernames?
+<<<<<<< Updated upstream
                     text = re.sub('(&amp;)|([^0-9A-Za-z\'@# ])|(\w+:\/\/\S+)', ' ', row[1])
                     # Get rid of excess spaces.
                     text = re.sub('(  +)', ' ', text)
@@ -54,13 +55,24 @@ def cleanse(target):
                     
                     row[1] = text
                     wtr.writerow(row)
+=======
+                    text = re.sub('(&amp;)|([^0-9A-Za-z\'@# \t])|(\w+:\/\/\S+)', ' ', row[1])
+
+                    # Remove any stopwords and single letters.
+                    text = ' '.join([term for term in text.split() if term not in STOP_WORDS and len(term) > 1])
+
+                    length_after += len(text)
+                    all_len_diffs.append((len(row[1]) - len(text)) / len(row[1]))
+
+                    wtr.writerow([row[0], text])
+>>>>>>> Stashed changes
     except IOError:
         log(f'Could not open file: {target}.csv!')
         sys.exit(1)
-    
+
     percent_reduction = round(((length_before - length_after) / length_before) * 100, 3)
     average_reduction = round((sum(all_len_diffs) / len(all_len_diffs) * 100), 3)
-    
+
     log(f'Stats for {target}:')
     log(f'\ttotal_tweets:\t\t{len(all_len_diffs)}')
     log(f'\tlength_before:\t\t{length_before}')
@@ -69,4 +81,3 @@ def cleanse(target):
     log(f'\tpercent_reduction:\t{percent_reduction}%')
     # Average percentage reduction per tweet.
     log(f'\taverage_reduction:\t{average_reduction}%')
-    
