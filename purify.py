db9ee6f9be89ceb56f35e0575dd1f9f580a01f0e
Qadir Haqq <haqq.q173@gmail.com>
2019-11-18 23:03:21 -0500

    Removed the period after the .csv in the open statement

--- purify.py
+++ purify.py
@@ -32,16 +32,16 @@ def cleanse(target):
         os.mkdir(DATA_DIR)
 
     with open(str(RAW_DIR / target) + '.csv', 'r', encoding='utf-8') as src_file:
-        with open(str(DATA_DIR / target) + '.csv.', 'w', newline='', encoding='utf-8') as dst_file:
+        with open(str(DATA_DIR / target) + '.csv', 'w', newline='', encoding='utf-8') as dst_file:
             rdr = csv.reader(src_file)
             wtr = csv.writer(dst_file)
-            
+
             for row in rdr:
                 length_before += len(row[1])
-                
-                # Block 1 removes '&amp;' as it is a specific nuisance. 
-                # Block 2 of this RE removes all usernames. 
-                # Block 3 removes all punctuation and non-alphanumeric 
+
+                # Block 1 removes '&amp;' as it is a specific nuisance.
+                # Block 2 of this RE removes all usernames.
+                # Block 3 removes all punctuation and non-alphanumeric
                 #   characters (except apostrophes, @ tags and hashtags).
                 # Block 4 removes URLs.
                 # text = re.sub('(&amp;)|(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)', ' ', row[1])
@@ -49,13 +49,13 @@ def cleanse(target):
                 text = re.sub('(&amp;)|([^0-9A-Za-z\'"\.,@# ])|(\w+:\/\/\S+)', ' ', row[1])
                 # Get rid of excess spaces.
                 text = re.sub('(  +)', ' ', text)
-                
+
                 length_after += len(text)
                 all_len_diffs.append((len(row[1]) - len(text)) / len(row[1]))
-                
+
                 row[1] = text
                 wtr.writerow(row)
-    
+
     percent_reduction = round(((length_before - length_after) / length_before) * 100, 3)
     average_reduction = round((sum(all_len_diffs) / len(all_len_diffs) * 100), 3)
 
